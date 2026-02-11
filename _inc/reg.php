f11c016a56a4d8dfca5aa249dfd0713b4be029f9
Geovane Bernich <geovane@gbernich.pro>
2026-02-10 21:12:02 -0300

    Fix Fatal Error: Correct mysqli_stmt_bind_param type string length in registration

--- _inc/reg.php
+++ _inc/reg.php
@@ -73,7 +73,7 @@ die("<script>self.location='?p=reg&erro=16'</script>");
         $senha_hash = password_hash($_POST['reg_senha'], PASSWORD_DEFAULT);
         $stmt_reg = mysqli_prepare($mysqli_link, "INSERT INTO usuarios (usuario, status, senha, email, personagem, vila, renegado, hunt_restantes, reg, natureza1, natureza2, natureza3, ip, vip, vip_inicio, ativador, yens) VALUES (?, 'ativo', ?, ?, ?, ?, ?, 14, ?, '', '', '', ?, ?, ?, ?, '9000')");
         $ip_long = ip2long($_SERVER['REMOTE_ADDR']);
-        mysqli_stmt_bind_param($stmt_reg, "ssssisssssss", $usuario, $senha_hash, $_POST['reg_email'], $personagem, $vila, $renegado, $atual, $ip_long, $vipadd, $atual, $novocodigo);
+        mysqli_stmt_bind_param($stmt_reg, "ssssissssss", $usuario, $senha_hash, $_POST['reg_email'], $personagem, $vila, $renegado, $atual, $ip_long, $vipadd, $atual, $novocodigo);
         mysqli_stmt_execute($stmt_reg) or die(mysqli_error($mysqli_link));
 
 
